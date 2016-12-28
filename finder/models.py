720e60afe7dce2fca6a51e6bef726d6b4f226a39
dretta <dretta@ymail.com>
2016-12-28 04:33:45 -0700

    Added Database models

---
+++ finder/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Country(models.Model):
+	code = models.CharField(primary_key=True, max_length=3) #ISO Alpha-3 Country Code
+	name = models.CharField(max_length=50, db_column="Name")
+
+
+class Embassy(models.Model):
+	government = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="government")
+	location = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="location")
+	name = models.CharField(max_length=200, db_column="Name")
+	street_address = models.CharField(max_length=200, db_column="Address")
+	city = models.CharField(max_length=50, db_column="City")
+	phone_number = models.IntegerField(default=-1, db_column="Phone Number")
+	fax_number = models.IntegerField(null=True, blank=True, db_column="Fax Number")
+	email_address = models.CharField(max_length=200, db_column="Email")
+	website = models.CharField(max_length=200, db_column="Link")
+
+	def __str__(self):
+		return self.name
