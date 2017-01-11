f488780ca9a5402f69aeb63ebd958e9fe3242dfc
dretta <dretta@ymail.com>
2017-01-11 01:26:09 -0700

    Embassy-Admin order

--- finder/admin.py
+++ finder/admin.py
@@ -4,6 +4,9 @@ from .models import Country, Embassy
 
 class CountryAdmin(admin.ModelAdmin):
     fields = ['name', 'code']
+	
+class EmbassyAdmin(admin.ModelAdmin):
+    fields = ['name', 'government', 'location', 'street_address', 'city', 'phone_number', 'fax_number', 'email_address', 'website']
 
 admin.site.register(Country, CountryAdmin)
-admin.site.register(Embassy)
+admin.site.register(Embassy, EmbassyAdmin)
