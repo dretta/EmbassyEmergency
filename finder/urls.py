f3a014fa800bea8057b54b7bccc253d30ac52519
dretta <dretta@ymail.com>
2017-01-20 18:06:07 -0700

    Fixed Generic View url issue

--- finder/urls.py
+++ finder/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 	# ex: / or /finder/
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	# ex: /finder/5/
-	url(r'^(?P<embassy_id>[0-9]+)/$', views.EmbassyView.as_view(), name='embassy_info'),
+	url(r'^(?P<pk>[0-9]+)/$', views.EmbassyView.as_view(), name='embassy_info'),
 	# ex: /finder/ABC/
-	url(r'^(?P<code>[A-Z]{3})/$', views.CountryView.as_view(), name='country_info'),
+	url(r'^(?P<pk>[A-Z]{3})/$', views.CountryView.as_view(), name='country_info'),
 	# ex: /finder/find/ABC/DEF
 	url(r'^find/(?P<government_code>[A-Z]{3})/(?P<location_code>[A-Z]{3})/$', views.results, name='results'),
 	# ex: /finder/find/ABC/DEF
