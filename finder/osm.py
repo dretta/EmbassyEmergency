0dd6b390bf33f1ad9a2ae71232caef95092d7a80
dretta <dretta@ymail.com>
2017-02-23 01:55:43 -0700

    Improved Query

--- finder/osm.py
+++ finder/osm.py
@@ -6,7 +6,7 @@ def getEmbassies():
 	api = overpy.Overpass()
 	# fetch all ways and nodes
 	result = api.query("""
-		[out:json];(node["amenity"="embassy"];way["amenity"="embassy"];relation["amenity"="embassy"]);out tags;
+		[out:json];(node["amenity"="embassy"]["country"]["target"];way["amenity"="embassy"]["country"]["target"];relation["amenity"="embassy"]["country"]["target"]);out tags;
 		""")
 		
 	return result
