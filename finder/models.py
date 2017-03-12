6ffa1e290c855ae805c3ccef550d82eb214d2b43
dretta <dretta@ymail.com>
2017-03-12 01:56:35 -0700

    Models turns off autoUpdate when manually updated

--- finder/models.py
+++ finder/models.py
@@ -39,6 +39,7 @@ def validate_string_all_caps(value):
 		
 class AutoUpdateModel(models.Model):
 	autoUpdate = models.BooleanField(default=True, db_column="Will Update via Query?")
+	start = None
 	
 	class Meta:
 		abstract=True
@@ -48,8 +49,19 @@ class Country(AutoUpdateModel):
 
 	code = models.CharField(primary_key=True, max_length=2, validators=[validate_nonempty, validate_string_length_three, validate_string_all_caps]) #ISO Alpha-3 Country Code
 	name = models.CharField(max_length=50, db_column="Name", validators=[validate_nonempty])
-	#autoUpdate = models.BooleanField(default=True, db_column="Will Update via Query?")
+	
+	@receiver(pre_save)
+	def pre_save_handler(sender, instance, *args, **kwargs):
+		instance.clean()
 
+	def clean(self):
+		if self.autoUpdate and self.start != (self.code, self.name):
+			self.autoUpdate = False
+		
+	def __init__(self, *args, **kwargs):
+		super(Country, self).__init__(*args, **kwargs)
+		self.start = (self.code,self.name)
+	
 	def __str__(self):
 		return self.name
 		
@@ -79,13 +91,18 @@ class Embassy(AutoUpdateModel):
 		if self.government == self.location:
 			raise ValidationError(
 				_('An embassy cannot have the same government and location.'))
+		if self.autoUpdate and self.start != (self.government,self.location,self.name,self.street_address,self.city,self.phone_number,self.fax_number,self.email_address,self.website):
+			self.autoUpdate = False
 		if not self.autoUpdate:
 			self.government.autoUpdate = False
 			self.government.save()
 			self.location.autoUpdate = False
 			self.location.save()
+
+	def __init__(self, *args, **kwargs):
+		super(Embassy, self).__init__(*args, **kwargs)
+		self.start = (self.government,self.location,self.name,self.street_address,self.city,self.phone_number,self.fax_number,self.email_address,self.website)
 			
-		
 	def __str__(self):
 		return self.name
 		
