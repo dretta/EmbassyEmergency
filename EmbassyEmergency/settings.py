ef7061c56bb8a2a698157101e5bc93fc72acbf82
dretta <dretta@ymail.com>
2017-07-01 19:38:21 -0600

    Disabled Debug setting

--- EmbassyEmergency/settings.py
+++ EmbassyEmergency/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ahp)5b^re7p-ntv3gbyp28%!j42$k+q*vi%rvg38nl&*=wej+r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
